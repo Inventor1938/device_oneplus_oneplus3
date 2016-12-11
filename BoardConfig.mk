fd166c3079a44469b541997a0b49edd8c24cdf6d
Martinusbe <martinusbe@gmail.com>
2016-12-11 22:36:37 +0100

    op3: add assert for 3t
    Change-Id: If6a4329883136b20fd64b97c61c13c0ba21f3de9
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@
 # Inherit from oppo-common
 -include device/oppo/common/BoardConfigCommon.mk
 
-TARGET_OTA_ASSERT_DEVICE := OnePlus3,oneplus3
+TARGET_OTA_ASSERT_DEVICE := OnePlus3,oneplus3,oneplus3t,OnePlus3t
 
 PLATFORM_PATH := device/oneplus/oneplus3
 
